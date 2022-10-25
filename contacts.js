7871351579cf9405dcb5da7c5c39bc7df4d9bdc6
guillermo <guilleburguillos@gmail.com>
2022-10-25 09:00:25 +0200

    subirtodo

---
+++ contacts.js
@@ -0,0 +1,24 @@
+let myLib = require("./person");
+
+class Contacts extends myLib.Person{
+
+    constructor(){
+
+        this.Personas = []
+    }
+
+printPerson(){
+
+    console.log()
+}
+
+
+}
+
+
+Personas.push(new myLib.Person("Javier", "Sánchez", 1990, 150, 56))
+
+console.log(Personas)
+
+
+
